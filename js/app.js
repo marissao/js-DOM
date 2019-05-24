4d264df62720e8a0d8bd6184d8393a402f1fcaaa
marissao <okazaki.marissa@gmail.com>
2019-05-23 23:27:11 -1000

    Completed Q1

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ Find the div with the id of "name1" and replace the n/a with the following:
 
 Tay-Tay*/
 
+var nameTaylor = document.getElementById('name1');
+nameTaylor.innerHTML = 'Tay-Tay'
 
 /*2. DJ Khaled
 
